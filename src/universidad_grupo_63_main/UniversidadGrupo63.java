ba1ff33c4970fec1ccf2dc2711191413910661fd
YaninaEstela <yaninaestelataylor@gmail.com>
2023-09-11 11:39:56 -0300

    conexion de base de datos
    se hizo la conexion a base de datos video 2

---
+++ src/universidad_grupo_63_main/UniversidadGrupo63.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package universidad_grupo_63_main;
+
+import java.sql.Connection;
+import universidad_grupo_63.accesoADatos.Conexion;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class UniversidadGrupo63 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // PARA SABER SI FUNCIONA LA CONEXIÓN
+        
+       Connection con = Conexion.getConexion();
+        
+       // PONÉS EN EL TECLADO SHIFT + F6 Y NO TENDRÍA QUE DAR ERROR
+       
+    }
+    
+}
